01100d16b9b4d19497e0217ee11c751e37d281ae
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-08 01:46:25 +0530

    added vertical timeline element in experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import SectionHeading from './section-heading'
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { experiencesData } from '@/lib/data';
 
 type Props = {}
 
 const Experience = (props: Props) => {
   return (
-    <div>Experience</div>
+    <section
+    id="experience"
+    > 
+      <SectionHeading>
+        My Experience
+      </SectionHeading>
+
+      <VerticalTimeline>
+        {
+          experiencesData.map((experience, index) =>(
+            <React.Fragment key={index}>
+              <VerticalTimelineElement>
+                
+              </VerticalTimelineElement>
+            </React.Fragment>
+          ))
+        }
+      </VerticalTimeline>
+    </section>
   )
 }
 
