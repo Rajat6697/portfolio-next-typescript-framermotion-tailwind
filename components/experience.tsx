4e58e73c79bfa024367979ffe844ca387130225a
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-08 02:04:11 +0530

    experience section final

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,33 +1,51 @@
-import React from 'react'
-import SectionHeading from './section-heading'
-import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { experiencesData } from '@/lib/data';
+import React from "react";
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
 
-type Props = {}
+type Props = {};
 
 const Experience = (props: Props) => {
   return (
-    <section
-    id="experience"
-    > 
-      <SectionHeading>
-        My Experience
-      </SectionHeading>
-
-      <VerticalTimeline>
-        {
-          experiencesData.map((experience, index) =>(
-            <React.Fragment key={index}>
-              <VerticalTimelineElement>
-                
-              </VerticalTimelineElement>
-            </React.Fragment>
-          ))
-        }
+    <section 
+    className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+   
+    id="experience">
+      <SectionHeading>My Experience</SectionHeading>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+            contentStyle={{
+              background : "#f3f4f6",
+              boxShadow : "none",
+              border : "1px solid rgba(0,0,0, 0.05)",
+              textAlign: "left",
+              padding : "1.3rem 2rem"
+            }}
+            contentArrowStyle={{
+              borderRight : "0.4rem solid"
+            }}
+            date={item.date}
+            icon={item.icon}
+            iconStyle={{
+              background : "white",
+              fontSize : "1.5rem",
+            }}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 font-normal text-gray-700">{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
       </VerticalTimeline>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
