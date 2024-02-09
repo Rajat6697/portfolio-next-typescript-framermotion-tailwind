4737fc5c2c2632d6ce7cbef38f1cc6ecce5c2b14
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-09 23:43:55 +0530

    added contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import SectionHeading from './section-heading'
 
 type Props = {}
 
 const Contact = (props: Props) => {
   return (
-    <div>Contact</div>
+    <section
+    className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+    id="contact">
+        <SectionHeading>
+            Contact Me
+        </SectionHeading>
+    <p>Please contact me directly at <a className='underline' href="mailto:rajatverma5338@gmail.com">rajatverma5338@gmail.com</a> or through this form</p>
+    </section>
   )
 }
 
