19f4b9aa471f2abc4e27eb8ed91076145f9ae8d9
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-09 23:46:05 +0530

    contact section minor

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import SectionHeading from './section-heading'
+import React from "react";
+import SectionHeading from "./section-heading";
 
-type Props = {}
+type Props = {};
 
 const Contact = (props: Props) => {
   return (
     <section
-    className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-    id="contact">
-        <SectionHeading>
-            Contact Me
-        </SectionHeading>
-    <p>Please contact me directly at <a className='underline' href="mailto:rajatverma5338@gmail.com">rajatverma5338@gmail.com</a> or through this form</p>
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      id="contact"
+    >
+      <SectionHeading>Contact Me</SectionHeading>
+      <p>
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:rajatverma5338@gmail.com">
+          rajatverma5338@gmail.com
+        </a>{" "}
+        or through this form
+      </p>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
