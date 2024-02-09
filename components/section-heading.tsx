6ba6d54b66a704715e02a79056db2254369a99a0
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-10 00:34:38 +0530

    added classes prop to sectionHeading for dynamic styling

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 type SectionHeadingProps = {
-    children: React.ReactNode 
+    children: React.ReactNode
+    classes?: string 
 }
 
-const SectionHeading = ({children} : SectionHeadingProps) => {
+const SectionHeading = ({children, classes=""} : SectionHeadingProps) => {
   return (
-    <h2 className='text-3xl font-medium capitalize mb-8'>{children}</h2>
+    <h2 className={`text-3xl font-medium capitalize mb-8 ${classes}`}>{children}</h2>
   )
 }
 
