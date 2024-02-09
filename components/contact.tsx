ad26b47a1a0d2e07801bb3107969e5d0a0d2aeea
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-10 00:37:38 +0530

    contact section minor

--- components/contact.tsx
+++ components/contact.tsx
@@ -8,22 +8,17 @@ import { motion } from "framer-motion";
 type Props = {};
 
 const Contact = (props: Props) => {
-
-  const {ref} = UseSectionInView(
-    HEADER_SECTIONS.CONTACT,
-      0.9
-    )
+  const { ref } = UseSectionInView(HEADER_SECTIONS.CONTACT, 0.9);
 
   return (
     <motion.section
-    ref={ref}
+      ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
       id="contact"
       initial={{ opacity: 0, y: 200 }}
-      whileInView={{opacity : 1 , y: 0 }} 
-      transition={{duration : .2}}
-      viewport={{once: true}}
-
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.2 }}
+      viewport={{ once: true }}
     >
       <SectionHeading classes="mb-2">Contact Me</SectionHeading>
       <p className="text-gray-700">
@@ -34,12 +29,24 @@ const Contact = (props: Props) => {
         or through this form
       </p>
 
-    <form className="mt-10 flex flex-col">
-      <input type="email" className="h-14 rounded-lg borderBlack p-4" placeholder="Your email"/>
-      <textarea  className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Your message here"/>
-      <button type="submit" className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] text-white bg-gray-900 rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950">Submit<FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 "/>{" "}</button>
-    </form>
-
+      <form className="mt-10 flex flex-col">
+        <input
+          type="email"
+          className="h-14 rounded-lg borderBlack p-4"
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message here"
+        />
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] text-white bg-gray-900 rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
+        >
+          Submit
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />{" "}
+        </button>
+      </form>
     </motion.section>
   );
 };
