eff6c6e45bea9259ded1062d5643dc25fb2f7902
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-10 01:00:33 +0530

    added server action SendEmail

---
+++ actions/SendEmail.ts
@@ -0,0 +1,10 @@
+"use server"
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export const sendEmail =(formData : FormData)=> {
+    console.log("Running on server")
+    console.log(formData.get("senderEmail"))
+    console.log(formData.get("message"))
+  }
