53bf1a0af4721d4c455596827a78d71882dae06e
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-13 00:42:56 +0530

    SendEmail action final

--- actions/SendEmail.ts
+++ actions/SendEmail.ts
@@ -1,10 +1,44 @@
 "use server"
+import { getErrorMessage, validateString } from "@/lib/utils"
 import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const sendEmail =(formData : FormData)=> {
+
+
+export const sendEmail = async (formData : FormData)=> {
     console.log("Running on server")
-    console.log(formData.get("senderEmail"))
-    console.log(formData.get("message"))
+    const senderEmail = formData.get("senderEmail");
+    const message = formData.get("message")
+
+    if(!validateString(senderEmail, 500)){
+        return {
+            status: 400,
+            error: "Invalid email"
+        }
+    }
+
+    if(!validateString(message, 500)){
+        return {
+            status: 400,
+            error: "Invalid email"
+        }
+    }
+
+    try {
+        await resend.emails.send({
+            from: "Rajat's Portfolio<onboarding@resend.dev>",
+            to: "rajatverma5338@gmail.com",
+            subject: "Message from portfolio contact form",
+            reply_to : senderEmail as string,
+            text: message as string
+        })
+    }
+    catch (error:unknown) {
+       return {
+        error : getErrorMessage(error)
+       }
+    }
+    
+  
   }
