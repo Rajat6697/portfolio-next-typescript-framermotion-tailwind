4adc1d6fa55a1ae27778168e55b4526036e5dd02
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-02-18 00:13:15 +0530

    SendEmail action updated

--- actions/SendEmail.ts
+++ actions/SendEmail.ts
@@ -1,10 +1,10 @@
 "use server"
+import React from "react"
 import { getErrorMessage, validateString } from "@/lib/utils"
 import { Resend } from "resend"
+import ContactFormEmail from "@/email/contact-form-email"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
-
-
 
 export const sendEmail = async (formData : FormData)=> {
     console.log("Running on server")
@@ -24,14 +24,17 @@ export const sendEmail = async (formData : FormData)=> {
             error: "Invalid email"
         }
     }
-
+    let data 
     try {
-        await resend.emails.send({
+       data =  await resend.emails.send({
             from: "Rajat's Portfolio<onboarding@resend.dev>",
             to: "rajatverma5338@gmail.com",
             subject: "Message from portfolio contact form",
             reply_to : senderEmail as string,
-            text: message as string
+            // react: React.createElement(ContactFormEmail, {
+            //     message : message as string,
+            //     senderEmail : senderEmail as string
+            // })
         })
     }
     catch (error:unknown) {
@@ -40,5 +43,8 @@ export const sendEmail = async (formData : FormData)=> {
        }
     }
     
+    return {
+        data
+    }
   
   }
